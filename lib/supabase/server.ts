ce9682f681ebed7b09fa2fa1680171a0b37a94c8
appsafevoice <appsafevoice@gmail.com>
2026-02-22 13:01:49 +0800

    changes

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers"
 export async function createClient() {
   const cookieStore = await cookies()
 
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "", {
+  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "https://zdypkxuhvdaetskceokv.supabase.co", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkeXBreHVodmRhZXRza2Nlb2t2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNzM0MzUsImV4cCI6MjA3OTc0OTQzNX0.pRjHBjSBlXEELWb8xdUvO21F0H4nD8eJ75CpmdCP9x0", {
     cookies: {
       getAll() {
         return cookieStore.getAll()
